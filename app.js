809f7fedef2e1968575dff865b7ec789ea503158
HelenLe <helenle8020@gmail.com>
2021-08-23 12:41:01 -0400

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app= express();
 
-var items=["Buy Food", "Cook Food"];
+var items=["Buy Food ", "Cook Food"];
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function (req,res){
